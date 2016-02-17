b6cf06575bffaa5c43aaa3fb7a96925fc2585545
Daisy Molving <daisymolving@gmail.com>
2016-02-17 10:54:59 +0000

    wrote new spec to test reverse_values method, test fails

--- stock_picker_spec.rb
+++ stock_picker_spec.rb
@@ -6,4 +6,10 @@ describe Stockpicker do
 	 result = stocks.reverse_values([1,7,3,9])
 	 expect(result).to eq [9,3,7,1]
   end
+
+  it 'reverses stock_values [10, 8, 4, 3] and returns [3,4,8,10]' do
+	 stocks = Stockpicker.new
+	 result = stocks.reverse_values([10,8,4,3])
+	 expect(result).to eq [3,4,8,10]
+  end
 end
